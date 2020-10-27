685c48f6a92151a281c8989477235c7f9eb7e332
saker <14163649+zhaoyang0415@users.noreply.github.com>
2020-10-27 23:33:47 +0800

    update xmind

--- main.go
+++ main.go
@@ -1,15 +1,4 @@
 package main
 
-import "fmt"
-
-type String string
-type MyString String
-
 func main() {
-	var s MyString = "hello"
-	ss := String(s)
-	sss := string(s)
-	fmt.Printf("%T\n", s)
-	fmt.Printf("%T\n", ss)
-	fmt.Printf("%T\n", sss)
 }
